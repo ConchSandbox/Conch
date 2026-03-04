63df8462574baed580abe78433b7f4217e179da1
chantsztung <petachan@huawei.com>
2026-03-04 10:07:35 +0800

    conchd: change listening port

--- cmd/conchd/main.go
+++ cmd/conchd/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/openeuler/Conch/internal"
 )
 
+const listenPort = "4063"
+
 func main() {
 	server, err := internal.NewServer()
 	if err != nil {
@@ -14,7 +16,7 @@ func main() {
 	defer server.Cleanup()
 
 	log.Println("starting conchd server")
-	if err := server.Start(":4063"); err != nil {
+	if err := server.Start(listenPort); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
